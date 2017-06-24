0788e2ecf2d7871791c4e18bfbbc6bdf663931eb
prabhusarath <prabhu.sarath@gmail.com>
2017-06-24 15:50:29 -0500

    Food-Project-Design

--- food-project/src/app/app.module.ts
+++ food-project/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './headerdata/headerdata.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ food-project/src/app/headerdata/headerdata.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headerdata',
+  templateUrl: './headerdata.component.html'})
+export class HeaderComponent {
+}
