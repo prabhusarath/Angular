fa5b169c7d60207304262bfce35c8883b24644e6
prabhusarath <prabhu.sarath@gmail.com>
2017-06-27 21:49:21 -0500

    Angular Emitter

--- food-project/src/app/app.component.ts
+++ food-project/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'app';
+  loadedOption = 'Recipes';
+
+  onSelection( features: string ) {
+    this.loadedOption = features;
+  }
 }
--- food-project/src/app/headerdata/headerdata.component.ts
+++ food-project/src/app/headerdata/headerdata.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-headerdata',
   templateUrl: './headerdata.component.html'})
 export class HeaderComponent {
+
+  @Output() sel= new EventEmitter<string>();
+
+  onSelect( optiond: string ) {
+    this.sel.emit(optiond);
+  }
 }
