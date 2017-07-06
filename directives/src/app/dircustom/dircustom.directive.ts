ca5649fa0619d97d96a10e828b8fe4a4c3e57148
prabhusarath <prabhu.sarath@gmail.com>
2017-07-06 13:15:34 -0500

    Angular Directives

--- directives/src/app/dircustom/dircustom.directive.ts
+++ directives/src/app/dircustom/dircustom.directive.ts
@@ -1,14 +1,22 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appDircustom]'
 })
 export class DircustomDirective implements OnInit {
 
+  @HostListener('mouseenter') mouseenter(eventData: Event) {
+    this.rend.setStyle(this.refs.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.rend.setStyle(this.refs.nativeElement, 'background-color', 'white');
+  }
+
   constructor(private refs: ElementRef, private rend: Renderer2) { }
 
   ngOnInit() {
-    this.rend.setStyle(this.refs.nativeElement, 'background-color', 'blue');
+   // this.rend.setStyle(this.refs.nativeElement, 'background-color', 'blue');
   }
 
 }
